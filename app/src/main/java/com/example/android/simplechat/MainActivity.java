307a78f116a99b3221a2e9f6f990b87fbb386cfe
scozzle <scozzle@gmail.com>
2020-04-25 12:01:04 -0700

    build.gradle config changes to enable FirebaseAuth. Enable email sign-in page to pop up at start.

--- app/src/main/java/com/example/android/simplechat/MainActivity.java
+++ app/src/main/java/com/example/android/simplechat/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.android.simplechat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Collections;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (shouldStartSignIn()) {
+            startSignIn();
+        }
+
+    }
+
+    private void startSignIn() {
+        // Sign in with FirebaseUI
+        Intent intent = AuthUI.getInstance().createSignInIntentBuilder()
+                .setAvailableProviders(Collections.singletonList(
+                        new AuthUI.IdpConfig.EmailBuilder().build()))
+                .setIsSmartLockEnabled(false)
+                .build();
+
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+    private boolean shouldStartSignIn() {
+        return (FirebaseAuth.getInstance().getCurrentUser() == null);
     }
 }
